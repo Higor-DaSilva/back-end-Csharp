0a01ed7ff5f2a5250da13bb6b2dbb32556a4137b
Higor Silva <higorx141@gmail.com>
2024-10-24 14:43:48 -0300

    Inicialização C#

---
+++ lacoDesafio01/Program.cs
@@ -0,0 +1,65 @@
+﻿// Uma certa empresa fez uma pesquisa de mercado com 10 pessoas para saber se
+
+// elas gostaram um determinado produto lançado. Para isso forneceu o sexo do
+
+// entrevistado e sua resposta (sim ou não). Faça um programa que calcule e imprima:
+
+// O número de pessoas que responderam SIM;
+// O número de pessoas que responderam NÃO;
+// O número de mulheres que responderam SIM;
+// A porcentagem de homens que responderam NÃO entre todos os homens analisados
+
+int homem = 0;
+int homemnao = 0;
+int mulher = 0;
+int mulhersim = 0;
+int respostasim = 0;
+int respostanao = 0;
+
+Console.WriteLine(@"
+   +--------------------------------------+
+   |      Bem-Vindo as industrias C.T     |
+   + -------------------------------------+
+   ");
+
+for (int n = 1; n <=10; n++)
+{
+    Console.WriteLine($"Qual seu sexo? m/f");
+    string sexo = Console.ReadLine()!;
+
+    Console.WriteLine($"Você gostou do produto? s/n");
+    string resposta = Console.ReadLine()!;
+
+    if(sexo == "m")
+    {
+        homem ++;
+    }
+    else 
+    {
+        mulher ++;
+    }
+
+    if(sexo == "f" && resposta == "s")
+    {
+        mulhersim ++;
+    }
+    if(sexo == "m" && resposta == "n")
+    {
+        homemnao ++;
+    }
+
+    if (resposta == "s")
+    {
+        respostasim ++;
+    }
+    else{
+        respostanao ++;
+    }
+}
+float percentual = (100 * homemnao) / homem;
+
+Console.WriteLine($"Total de pessoas que dizeram 'sim' é {respostasim}");
+Console.WriteLine($"Total de pessoas que dizeram 'não' é {respostanao}");
+Console.WriteLine($"Total de mulheres que dizeram 'sim' é {mulhersim}");
+Console.WriteLine($"Percentual de homens que dizeram 'não' é {percentual}%");
+
--- lacosEx01/Program.cs
+++ lacosEx01/Program.cs
@@ -1,8 +1,10 @@
 ﻿//faca um programa que rode 10 vezes e pergunte o sexo da pessoa e ao final informe
 //qunatidade de pessoas de sexo feminino e masculino
 
-int homem= 0;
+int homem = 0;
 int mulher = 0;
+int sport = 0;
+int naogosta = 0;
 
 
 Console.WriteLine(@"
@@ -11,20 +13,45 @@ Console.WriteLine(@"
    + -------------------------------------+
    ");
 
-for (int n = 1; n <= 10; n++)
-{
-    Console.WriteLine("INforme seu sexo(masculino ou feminino):");
-    string sexo = Console.ReadLine()!;
+Console.WriteLine($"Quantas pessoas tem na sua mesa?");
+int pessoas = int.Parse(Console.ReadLine()!);
 
-    if(sexo == "masculino")
+
+for (int n = 1; n <= pessoas; n++)
+{
+    Console.WriteLine("INforme seu sexo(m / f)");
+    string sexo = Console.ReadLine()!;
+    Console.WriteLine("voce gosta de esportes(s/n)?");
+    string esporte = Console.ReadLine()!;
+
+
+    if (sexo == "m")
     {
-        homem ++;
+        homem++;
     }
     else
     {
-        mulher ++;
+        mulher++;
     }
+
+    if (esporte == "s")
+    {
+        sport++;
+    }
+    else
+    {
+        naogosta++;
+    }
+
+
 }
+int entrevistados = (mulher + homem);
+float percentual = (100 * sport) / entrevistados;
 
 Console.WriteLine($"A quantidade de homens é {homem}");
 Console.WriteLine($"A quantidade de mulheres é {mulher}");
+Console.WriteLine($"A quantidade de pessoa que gostam de esporte é {sport}");
+Console.WriteLine($"A quantidade de pessoa que nao gostam de esporte é {naogosta}");
+Console.WriteLine($"A quantidade de entrevistados é {entrevistados}");
+Console.WriteLine($"Percentual de pessoa que gostam de esportes é {percentual}%");
+
