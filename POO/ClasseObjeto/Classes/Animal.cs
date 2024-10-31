8e1ae196a462d9ced3c3538df7a6f2e01aeec964
Higor Silva <higorx141@gmail.com>
2024-10-31 16:46:13 -0300

    Classes

---
+++ POO/ClasseObjeto/Classes/Animal.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseObjeto.Classes
+{
+    public class Animal
+    {
+        public string raca;
+        public string cor;
+        public string name;
+        public int idade;
+    }
+
+    public class Gato
+    {
+        public string name;
+        public string raca;
+        public int idade;
+        public string cor;
+    }
+}
---
+++ POO/ClasseObjeto/Program.cs
@@ -0,0 +1,30 @@
+﻿//Importando as classes de dentro da pasta Classes
+using System.Net.Http.Headers;
+using ClasseObjeto.Classes;
+
+//cria o objeto/variavel animal
+Animal cachorro = new Animal();
+Gato gato = new Gato();
+
+cachorro.name = "Apollo";
+cachorro.raca = "Labrador";
+cachorro.cor = "Amarelo";
+cachorro.idade = 4 ;
+
+gato.name = "Bolota";
+gato.raca = "Rua";
+gato.cor = "Branca com manchas";
+gato.idade = 6 ;
+
+Console.WriteLine($"Nome do cachorro: {cachorro.name}");
+Console.WriteLine($"Raça do {cachorro.name}: {cachorro.raca}");
+Console.WriteLine($"Cor do {cachorro.name}: {cachorro.cor}");
+Console.WriteLine($"Idade do {cachorro.name}: {cachorro.idade}");
+
+System.Console.WriteLine();
+
+Console.WriteLine($"Nome do Gata: {gato.name}");
+Console.WriteLine($"Nome do {gato.name}: {gato.cor}");
+Console.WriteLine($"Nome do {gato.name}: {gato.idade}");
+Console.WriteLine($"Nome do {gato.name}: {gato.raca}");
+
